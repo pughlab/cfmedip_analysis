6203ea0d302c436c682bd7a084ed053bef98d60f
Suluxan Mohanraj <suluxan.m@gmail.com>
2019-06-20 11:10:00 -0400

    Initial commit

---
+++ bed_to_matrix.R
@@ -0,0 +1,42 @@
+library(optparse)
+library(Matrix)
+
+# optparse
+option_list <- list(make_option(c("-i", "--input"), default="NA"))
+opt <- parse_args(OptionParser(option_list=option_list))
+
+ListMandatory<-list("input")
+for (param in ListMandatory) {
+  if (length(grep('^NA$',opt[[param]], perl = T))) {
+    stop(paste("Parameter -", param, " can't be 'NA.", sep = "", collapse = ""))
+  }
+}
+
+# pre-processing
+wigbed <- opt$input
+x <- read.table(wigbed,header=F,sep="\t")
+wigbed_basename <- basename(wigbed)
+
+y <- data.frame(data.frame(paste(x[,1], x[,2], x[,3], sep=".")),x[,5])
+x2 <- data.frame(y[,-1])
+colnames(x2) <- c(wigbed_basename)
+rownames(x2) <- y[,1]
+
+colnames(y) <- c("bins",wigbed_basename)
+
+# matrix and sparse matrix
+mat1 <- as.matrix(x2)
+print("Total count of 0s in matrix:")
+colSums(mat1 == 0)
+
+print("Percentage of 0s in matrix:")
+(colSums(mat1 == 0)/nrow(mat1))*100
+
+#sparse_mat1 <- Matrix(mat1, sparse = TRUE)
+saveRDS(mat1, file=(paste0(wigbed_basename,"_matrix.rds")))
+
+# plot
+png(paste0("plots/",wigbed_basename,".png"))
+hist(mat1, xlim=c(0,50), breaks=1000000, col="black")
+dev.off()
+
---
+++ merge_matrices.R
@@ -0,0 +1,33 @@
+library(optparse)
+library(Matrix)
+require(data.table)
+require(tidyverse)
+
+# optparse
+option_list <- list(make_option(c("-i", "--input"), default="NA"), make_option(c("-s","--sample_id", default="HCC_all")))
+opt <- parse_args(OptionParser(option_list=option_list))
+
+ListMandatory<-list("input")
+for (param in ListMandatory) {
+  if (length(grep('^NA$',opt[[param]], perl = T))) {
+    stop(paste("Parameter -", param, " can't be 'NA.", sep = "", collapse = ""))
+  }
+}
+
+# merge pre-processing
+workdir <- opt$input
+sampleid <- opt$sample_id
+setwd(workdir)
+
+files <- list.files(path = workdir, pattern = "\\.rds$", full.names = TRUE)
+data_list = lapply(files, function (x) data.table(readRDS(x)))
+hcc_final <- reduce(data_list, full_join, by = "bins")
+
+x2 <- data.frame(hcc_final[,-1])
+rownames(x2) <- hcc_final[,1]
+
+# matrix and sparse matrix
+mat1 <- as.matrix(x2)
+saveRDS(mat1, file=(paste0(sampleid,"_final_fullmatrix.rds")))
+#sparse_mat1 <- Matrix(mat1, sparse = TRUE)
+#saveRDS(sparse_mat1, file=(paste0(sampleid,"_final_sparsematrix.rds")))
