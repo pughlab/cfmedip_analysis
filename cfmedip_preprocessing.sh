e0c87fa2bc1da13ce23c89dfdd6cfd6d667adf7b
Suluxan Mohanraj <suluxan.m@gmail.com>
2019-06-20 11:53:49 -0400

    working directory only needs to be added once

--- cfmedip_preprocessing.sh
+++ cfmedip_preprocessing.sh
@@ -6,7 +6,9 @@ module load bedops
 module load R
 
 # directory with the MEDIPS outputs i.e. files ending in .wig
-cd /mnt/work1/users/home2/smohanra/pughlab/projects/BAMgineer/kui/HCC_Counts_wig/wigs-backup_no_span_change
+workingdir="/mnt/work1/users/home2/smohanra/pughlab/projects/BAMgineer/kui/HCC_Counts_wig/wigs-backup_no_span_change"
+
+cd $workingdir
 mkdir -p plots
 
 # Loop through all the wig files
@@ -23,4 +25,4 @@ done
 # this Rscript is for merging multiple matrices together
 # -i is the directory where the .rds files to be merged are (generated from the previous step)
 # -s is a sample id to name the final full matrix (default is "HCC_all") 
-Rscript merge_matrices.R -i /mnt/work1/users/home2/smohanra/pughlab/projects/BAMgineer/kui/HCC_Counts_wig/wigs-backup_no_span_change -s HCC_all
+Rscript merge_matrices.R -i $workingdir -s HCC_all
